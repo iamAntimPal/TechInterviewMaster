3a53d31a38e5b261f4b75dee5feee5b081074f82
Antim Pal <134076504+iamAntimPal@users.noreply.github.com>
2025-04-17 23:59:02 +0530

    Create addition.py
    Co-Authored-By: Antim-IWP <203163676+Antim-IWP@users.noreply.github.com>

---
+++ Interview/Python/Solutions/140+ Basic Python Programs/Program2/addition.py
@@ -0,0 +1,10 @@
+# Write a Python program to do arithmetical operations addition and division.
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Addition
+sum = num1 + num2
+print("The sum of {0} and {1} is {2}".format(num1, num2, sum))
+# Division
+div = num1 / num2
+print("The division of {0} and {1} is {2}".format(num1, num2, div))
