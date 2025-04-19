45899f521b3e4fe8a298395270c881a4bb0a7d91
Antim Pal <134076504+iamAntimPal@users.noreply.github.com>
2025-04-19 13:48:29 +0530

    Create km_to_miles.py
    Co-Authored-By: Antim-IWP <203163676+Antim-IWP@users.noreply.github.com>

---
+++ Interview/Python/Solutions/140+ Basic Python Programs/Program 6/km_to_miles.py
@@ -0,0 +1,6 @@
+# Python Program to convert kilometers to miles
+km = float(input("Enter vlaue in kilomiters:"))
+miles = 0.621371192 * km
+print("The value in miles is:", miles)
+print("The value in miles is: %.2f" % miles)
+print("The value in miles is: {:.2f}".format(miles))
