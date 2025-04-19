03c89dece782cf3c521c88cd9617da94fea41dea
Antim Pal <134076504+iamAntimPal@users.noreply.github.com>
2025-04-19 16:36:35 +0530

    Update odd_even_checker.py
    Co-Authored-By: Antim-IWP <203163676+Antim-IWP@users.noreply.github.com>

--- Interview/Python/Solutions/140+ Basic Python Programs/Program 12/odd_even_checker.py
+++ Interview/Python/Solutions/140+ Basic Python Programs/Program 12/odd_even_checker.py
@@ -2,9 +2,6 @@
 # Author: Antim Pal
 # Date : 2025-04-19
 # Description: This program checks if a number is odd or even.
-# It takes an integer input from the user and checks if it is odd or even.
-# It then prints the result to the console.
-# The program uses the modulus operator to determine if the number is odd or even.
 
 a = int(input("Enter a number: "))
 if a % 2 == 0:
