a957f91af905d8cff38465dabca11f77a375b6e9
Antim Pal <134076504+iamAntimPal@users.noreply.github.com>
2025-04-19 21:44:51 +0530

    Update prinme_checker2.py
    Co-Authored-By: Antim-IWP <203163676+Antim-IWP@users.noreply.github.com>

--- Interview/Python/Solutions/140+ Basic Python Programs/Program 14/prinme_checker2.py
+++ Interview/Python/Solutions/140+ Basic Python Programs/Program 14/prinme_checker2.py
@@ -3,20 +3,3 @@
 # Problem: Write a program to check whether a number is prime or not.
 # Approach: A prime number is a number that is only divisible by 1 and itself.
 
-num = int(input("Enter a number: "))
-# define a flag variable
-flag = False
-if num == 1:
- print(f"{num}, is not a prime number")
-elif num > 1:
- # check for factors
- for i in range(2, num):
- if (num % i) == 0:
- flag = True # if factor is found, set flag to True
- # break out of loop
- break
- # check if flag is True
-if flag:
- print(f"{num}, is not a prime number")
-else:
- print(f"{num}, is a prime number")
