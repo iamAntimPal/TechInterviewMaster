6db145270dcc222363cb71450a91f5fb7ee75db9
Antim Pal <134076504+iamAntimPal@users.noreply.github.com>
2025-04-19 16:03:47 +0530

    Update check_number.py
    Co-Authored-By: Antim-IWP <203163676+Antim-IWP@users.noreply.github.com>

---
+++ Interview/Python/Solutions/140+ Basic Python Programs/Program 11/check_number.py
@@ -0,0 +1,12 @@
+# Python program to check if the number is positive, negative or zero
+
+# Input from user
+num = float(input("Enter a number: "))
+
+# Check and display result
+if num > 0:
+    print("The number is Positive.")
+elif num == 0:
+    print("The number is Zero.")
+else:
+    print("The number is Negative.")
