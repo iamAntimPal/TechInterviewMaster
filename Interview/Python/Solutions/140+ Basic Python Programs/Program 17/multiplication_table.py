ed7ced4fa559dabb99f49b2577127a1e751fc475
Antim Pal <134076504+iamAntimPal@users.noreply.github.com>
2025-04-20 03:39:00 +0530

    adding

--- Interview/Python/Solutions/140+ Basic Python Programs/Program 17/multiplication_table.py
+++ Interview/Python/Solutions/140+ Basic Python Programs/Program 17/multiplication_table.py
@@ -25,8 +25,7 @@ for i in range(1, 11):
 print(f"Multiplication table of {num} using while loop:")
 i = 1
 while i <= 10:
-     print(f"{num} x {i} = {num*i}")
-     i += 1
+     print(f"{num} x {i} = {num*i}")     i += 1
 
 
 
